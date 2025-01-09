b64ea6743bfd52943fda05aba8580765065b52f0
PatNowak <patryk@sirvoy.com>
2025-01-09 12:34:19 +0100

    docker compose, not docker-compose

--- app.sh
+++ app.sh
@@ -3,20 +3,20 @@
 # first argument
 case "$1" in
     "status")
-      docker compose -f docker-compose-redis.yml ps -a
-      docker compose -f docker-compose-valkey.yml ps -a
+      docker compose -f docker compose-redis.yml ps -a
+      docker compose -f docker compose-valkey.yml ps -a
     ;;
 
     "redis")
-      docker-compose -f docker-compose-redis.yml up -d
+      docker-compose -f docker compose-redis.yml up -d
     ;;
     "valkey")
-      docker-compose -f docker-compose-valkey.yml up -d
+      docker-compose -f docker compose-valkey.yml up -d
     ;;
 
     "down")
-      docker compose -f docker-compose-valkey.yml down --volumes --rmi=all
-      docker compose -f docker-compose-redis.yml down --volumes --rmi=all
+      docker compose -f docker compose-valkey.yml down --volumes --rmi=all
+      docker compose -f docker compose-redis.yml down --volumes --rmi=all
     ;;
 
     "6.4")
